49d4d0a408d2dd9d92fabdc98936f8f6b59b46a2
Antonio MG <tonio.mg@gmail.com>
2012-03-05 19:13:43 +0100

    Initial Commit

---
+++ remotePong/RPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RPAppDelegate.h
+//  remotePong
+//
+//  Created by Antonio MG on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ remotePong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  remotePong
+//
+//  Created by Antonio MG on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RPAppDelegate class]));
+    }
+}
