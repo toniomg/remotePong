d8402dc4120d0f6c1223b6529e80362f9d4a529b
Antonio MG <toniomg@Antonio-MGs-Mac-Pro.local>
2012-04-07 17:07:01 +0200

    first commit

---
+++ remotePong/RPSprite.h
@@ -0,0 +1,13 @@
+//
+//  RPSprite.h
+//  BouncingBall
+//
+//  Created by Antonio MG on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPSprite : NSObject
+
+@end
---
+++ remotePong/RPSprite.m
@@ -0,0 +1,13 @@
+//
+//  RPSprite.m
+//  BouncingBall
+//
+//  Created by Antonio MG on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RPSprite.h"
+
+@implementation RPSprite
+
+@end
